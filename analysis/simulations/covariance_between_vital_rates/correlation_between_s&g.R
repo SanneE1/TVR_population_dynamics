ba2e97480f56a2e8350236da063650c7668a9c95
Sanne <sanne.evers@idiv.de>
2020-10-28 11:58:28 +0100

    calculate correlation, variance and covariance of s and g matrices with covarying climate

--- analysis/simulations/covariance_between_vital_rates/correlation_between_s&g.R
+++ analysis/simulations/covariance_between_vital_rates/correlation_between_s&g.R
@@ -4,12 +4,15 @@ library(parallel)
 library(pbapply)
 library(patchwork)
 
-# Transforms all values below/above limits in min/max size
-
+#------------------------------------
+# IPM functions
+#------------------------------------
 
 # Survival function
 s_z <- function(z, pars, clim) {
-  x <- pars$s_int + pars$s_size * log(z) + pars$s_temp * clim$surv
+  if(pars$species == "H") { z <- log(z) }
+  
+  x <- pars$s_int + pars$s_size * z + pars$s_temp * clim$surv
   p <- 1/(1+exp(-(x)))
 
   return(p)
@@ -18,33 +21,52 @@ s_z <- function(z, pars, clim) {
 # Growth function
 g_z1z <- function(z1, z, pars, clim) {
 
-
-  mean <- exp(pars$g_int + pars$g_size * log(z) + pars$g_temp * clim$growth)  ## mean size next year
-  p_grow <- dnorm(z1, mean = mean, sd = pars$g_sd)   ### probability distribution that you grow to size z1
+  if(pars$species == "H") {
+    mean <- exp(pars$g_int + pars$g_size * log(z) + pars$g_temp * clim$growth) 
+  } else if(pars$species == "O") {
+    mean <- pars$g_int + pars$g_size * z + pars$g_temp * clim$growth
+  }
+  ev <- (pnorm(pars$U, mean, pars$g_sd) - pnorm(pars$L, mean, pars$g_sd)) ## Eviction correction
+  
+  p_grow <- dnorm(z1, mean = mean, sd = pars$g_sd)  / ev
+  
   ### based on mean size and sd
   return(p_grow)
 }
 
+# Survival * Growth
+P_z1z <- function(z1, z, pars) {
+  p <- s_z(z, pars) * g_z1z(z1, z, pars)
+  return(p)
+}
 
 # Flower probability
 fp_z <- function(z, pars) {
-
-  x <- pars$fp_int + pars$fp_size * log(z)
+  
+  if(pars$species == "H") { z <- log(z)  }
+  x <- pars$fp_int + pars$fp_size * z
   p <- 1/(1+exp(-(x)))
 
   return(p)
 }
-# Seed production
-seed_z <- function(z, pars, species = "H") {
-  
-  if(species == "H") {
-  n <- exp(pars$seed_int + pars$seed_size * log(z))
-  } else if(species == "O") {
-    n <- rnorm(1, pars$seed_int, pars$seed_size)
-  }
 
+# Flower numbers
+fn_z <- function(z, pars) {
+  
+  if(pars$species == "H") { z <- log(z)  }
+  fn <- exp(pars$fn_int + pars$fn_size * z)
+  
+  return(fn)
+}
+
+# Seed production
+seed_z <- function(z, pars) {
+  
+  n <- rnorm(1, pars$seed_int, pars$seed_size)
+  
   return(n)
 }
+
 # recruit size
 fd_z1 <- function(z1, pars) {
 
@@ -54,12 +76,17 @@ fd_z1 <- function(z1, pars) {
 }
 
 # Fecundity function
-fec <- function(z1, z, pars, species) {
-  fec <- fp_z(z, pars) * seed_z(z, pars, species) * pars$germ_int * fd_z1(z1, pars)
+fec <- function(z1, z, pars) {
+  fec <- fp_z(z, pars) * fn_z(z, pars) * seed_z(z, pars) * pars$germ_int * fd_z1(z1, pars)
   return(fec)
 }
 
-# HEQU
+
+#------------------------------------
+# Vital Rate Parameters
+#------------------------------------
+
+# HEQU based
 # pars <- data.frame(species = "H",
 #                    s_int = -0.229,
 #                    s_size = 1.077,
@@ -72,10 +99,12 @@ fec <- function(z1, z, pars, species) {
 #                    fp_size = 1.719,
 #                    fd_int = 1.178749,
 #                    fd_sd = 0.76,
-#                    seed_int = -0.6652477,
-#                    seed_size = 0.8747764,
-#                    germ_int = 0.1262968,
-#                    germ_sd = 0.2725941,
+#                    fn_int = -0.646,
+#                    fn_size = 0.705,
+#                    seed_int = 60.4,
+#                    seed_size = 5,
+#                    germ_int = 0.00229,
+#                    germ_sd = 0.00444,
 # 
 #                    mat_siz = 100,
 #                    L = 1,
@@ -84,32 +113,37 @@ fec <- function(z1, z, pars, species) {
 # )
 
 
-# MEROW example
+# # OPIM based
 pars <- data.frame(species = "O",
-                   s_int = -0.364,
-                   s_size = 0.632,
+                   s_int = 0.384,
+                   s_size = 0.381,
                    s_temp = -0.4,
-                   g_int = 1.497,
-                   g_size = 0.0635,
+                   g_int = 0.435,
+                   g_size = 0.955,
                    g_temp = -0.746,
-                   g_sd = 1.778,
-                   fp_int = -5.113,
-                   fp_size = 2.017,
+                   g_sd = 0.2,
+                   fp_int = -18.996,
+                   fp_size = 1.731,
                    fd_int = -3.25,
                    fd_sd = 0.77,
+                   fn_int = -6.738,
+                   fn_size = 0.771,
                    seed_int = 139.5,
                    seed_size = 10.6,
-                   germ_int = -3.43,
+                   germ_int = 0.0314,
                    germ_sd = 0.21,
-                   
+
                    mat_siz = 100,
-                   L = 1,
-                   U = 115
-                   
+                   L = -3.8,
+                   U = 18
+
 )
 
+#------------------------------------
+# IPM kernels
+#------------------------------------
 
-kernel <- function(pars, clim, species){
+kernel <- function(pars, clim){
 
   n   <- pars$mat_siz
   L   <- pars$L
@@ -122,7 +156,7 @@ kernel <- function(pars, clim, species){
 
   # Fertility matrix
   Fmat <- matrix(0,(n),(n))
-  Fmat <- outer(y, y, fec, pars, species) * h
+  Fmat <- outer(y, y, fec, pars) * h
 
 
   # Survival matrix
@@ -135,19 +169,9 @@ kernel <- function(pars, clim, species){
 
 
   # Growth/survival matrix
-  Tmat <- gmat # place holder
-
-  # fix eviction of offspring
-  for(i in 1:(pars$mat_siz/2)) {
-    gmat[1,i]<- gmat[1,i]+1-sum(gmat[,i])
-    Tmat[,i]<-gmat[,i]*smat[i]
-  }
-  # fix eviction of large adults
-  for(i in (pars$mat_siz/2+1):pars$mat_siz) {
-    gmat[n,i]<-gmat[n,i]+1-sum(gmat[,i])
-    Tmat[,i]<-gmat[,i]*smat[i]
-  }
-
+  Tmat <- t(t(gmat) * smat) # place holder
+  
+  
   # Full kernel
   k_yx <- Fmat + Tmat
 
@@ -164,8 +188,8 @@ kernel <- function(pars, clim, species){
 # Calculate variance of, and correlation between survival and growth matrices
 #----------------------------------------------------------------------------------------------------------------
 
-calc_corr_lambda <- function(clim_corr) {
-  clim_seq <- rnorm_multi(n = 5000, mu = c(0,0), sd = 2, r = clim_corr, varnames = c("surv", "growth"))
+calc_corr_lambda <- function(clim_cov) {
+  clim_seq <- rnorm_multi(n = 5000, mu = c(0,0), sd = 2, r = clim_cov, varnames = c("surv", "growth"))
   init_pop_vec <- runif(pars$mat_siz)
 
 
@@ -182,7 +206,7 @@ calc_corr_lambda <- function(clim_corr) {
       pop = init_pop_vec
     } else {pop = pop_vec[[i-1]]}
 
-    k <- kernel(pars, clim_seq[i,], pars$species)
+    k <- kernel(pars, clim_seq[i,])
     pop_vec[[i]] <- k$k_yx %*% pop
     smats[[i]] <- k$smat
     gmats[[i]] <- k$gmat
@@ -197,7 +221,7 @@ calc_corr_lambda <- function(clim_corr) {
   c <- data.frame(T1 = a,T2 = c(b, NA))
   c$lambda <- log(c$T2 / c$T1)
 
-  d <- c[-c(1:(0.1 * length(c$lambda))),] %>%
+  d <- c %>%
     filter(is.finite(lambda))
 
 
@@ -209,9 +233,9 @@ calc_corr_lambda <- function(clim_corr) {
 
   pb <- txtProgressBar(min = 0, max = length(pars$mat_siz), style = 1)
   for (c in c(1:pars$mat_siz)) {
-    s <- sapply(smats, function(x) x[c])
+    s <- sapply(smats, function(x) x[c])[-c(1:500)]       # remove the first 10% to remove effect of initiation population
     for (r in c(1:pars$mat_siz)) {
-      g <- sapply(gmats, function(x) x[r,c])
+      g <- sapply(gmats, function(x) x[r,c])[-c(1:500)]   # remove the first 10% to remove effect of initiation population
 
       correlations[r,c] <- cor(s,g)
       variance_s[r,c] <- var(s)
@@ -223,7 +247,7 @@ calc_corr_lambda <- function(clim_corr) {
 
   }
 
-  return(tibble(clim_corr = clim_corr,
+  return(tibble(clim_cov = clim_cov,
                 lambda = d$lambda %>% mean(na.rm = T),
                 correlation = list(correlations),
                 variance_s = list(variance_s),
@@ -237,13 +261,14 @@ cl <- makeCluster(cores-2)
 clusterExport(cl = cl, ls())
 clusterEvalQ(cl, c(library(faux), library(ipmr), library(dplyr)))
 
-lambdas <- pblapply(as.list(rep(c(-0.9,0,0.9), 15)), calc_corr_lambda, cl = cl) %>% bind_rows
+lambdas <- pblapply(as.list(rep(c(-0.9,0,0.9), 30)), calc_corr_lambda, cl = cl) %>% bind_rows
 
 stopCluster(cl)
 
-saveRDS(lambdas, file = "results/simulations/correlation_between_s&g.R")
+saveRDS(lambdas, 
+        file = paste("results/simulations/correlation_between_s&g_", pars$species, ".rds", sep = ""))
 
-lambdas <- readRDS("results/simulations/correlation_between_s&g.rds")
+lambdas <- readRDS(paste("results/simulations/correlation_between_s&g_", pars$species, ".rds", sep = ""))
 
 
 mat_cor <- matrix(0, (pars$mat_siz), (pars$mat_siz))
@@ -256,8 +281,8 @@ variance_s <- list()
 variance_g <- list()
 covariance <- list()
 
-for (i in unique(lambdas$clim_corr)) {
-  df <- lambdas[which(lambdas$clim_corr == i),]
+for (i in unique(lambdas$clim_cov)) {
+  df <- lambdas[which(lambdas$clim_cov == i),]
   for (c in c(1:pars$mat_siz)) {
     for (r in c(1:pars$mat_siz)) {
 
@@ -283,10 +308,27 @@ names(variance_s) <- c("neg", "zero", "pos")
 names(variance_g) <- c("neg", "zero", "pos")
 names(covariance) <- c("neg", "zero", "pos")
 
-saveRDS(correlations, file = "results/simulations/correlation_between_s&g_correlation.rds")
-save(variance_s, variance_g, file = "results/simulations/correlation_between_s&g_variance.RData")
-saveRDS(covariance, file = "results/simulations/correlation_between_s&g_covariance.rds")
+saveRDS(correlations, file = paste("results/simulations/correlation_between_s&g_", pars$species, "correlation.rds", sep = ""))
+save(variance_s, variance_g, file = paste("results/simulations/correlation_between_s&g_", pars$species, "variance.RData", sep = ""))
+saveRDS(covariance, file = paste("results/simulations/correlation_between_s&g_", pars$species, "covariance.rds", sep = ""))
 
+
+#--------------------------------------------------------
+# Plot effect of covariance on lambda
+#--------------------------------------------------------
+
+
+# car::leveneTest(lambda ~ as.factor(clim_cov), data = lambdas)  ## No difference in variance, anova allowed
+
+lam <- ggplot(lambdas, aes(x = as.factor(clim_cov), y = lambda)) + 
+  geom_boxplot() + ggpubr::stat_compare_means(method = "anova") + 
+  xlab("Climate covariance") +
+  ggtitle("Effect of climate covariance on lambda", 
+          subtitle = "with a climate sd of 2") 
+
+
+ggsave(filename = paste("results/simulations/correlation_between_s&g_", pars$species, "_lambdas.png", sep = ""), 
+       lam)
 
 #--------------------------------------------------------
 # Plot correlation between survival and growth matrices
@@ -305,7 +347,9 @@ a <- lapply(correlations, function(x) x %>%
               tibble::rowid_to_column(var = "X") %>% 
               gather(key = "Y", value = "corr", -1) %>%
               mutate(Y = as.numeric(gsub("V", "", Y))) %>%
-              ggplot(aes(X, Y, fill = corr)) + geom_tile() + theme_minimal() + coord_flip() + scale_x_reverse()
+              ggplot(aes(X, Y, fill = corr)) + geom_tile() + 
+              scale_fill_gradientn(colours = c("blue", "white", "red")) +
+              theme_minimal() + coord_flip() + scale_x_reverse()
 )
 
 plot_cor <- ((a$neg + ggtitle("-0.9")) + 
@@ -314,7 +358,8 @@ plot_cor <- ((a$neg + ggtitle("-0.9")) +
   plot_annotation(title = "Correlation between survivial and growth kernels",
                   subtitle = "with climate covariance")
 
-ggsave(filename = "results/Correlation_s_g_correlation_OPIM.png", plot_cor, width = 18, height = 7)
+ggsave(filename = paste("results/simulations/correlation_between_s&g_", pars$species, "_correlation.png", sep = ""), 
+       plot_cor, width = 18, height = 7)
 
 
 
@@ -343,7 +388,9 @@ b <- lapply(variance_s, function(x) x %>%
               tibble::rowid_to_column(var = "X") %>% 
               gather(key = "Y", value = "var", -1) %>%
               mutate(Y = as.numeric(gsub("V", "", Y))) %>%
-              ggplot(aes(X, Y, fill = var)) + geom_tile() + theme_minimal() + coord_flip() + scale_x_reverse()
+              ggplot(aes(X, Y, fill = var)) + geom_tile() +
+              scale_fill_gradientn(colours = c("blue", "white", "red")) + 
+              theme_minimal() + coord_flip() + scale_x_reverse()
 )
 
 c <- lapply(variance_g, function(x) x %>% 
@@ -351,7 +398,9 @@ c <- lapply(variance_g, function(x) x %>%
               tibble::rowid_to_column(var = "X") %>% 
               gather(key = "Y", value = "var", -1) %>%
               mutate(Y = as.numeric(gsub("V", "", Y))) %>%
-              ggplot(aes(X, Y, fill = var)) + geom_tile() + theme_minimal() + coord_flip() + scale_x_reverse()
+              ggplot(aes(X, Y, fill = var)) + geom_tile() + 
+              scale_fill_gradientn(colours = c("blue", "white", "red")) +
+              theme_minimal() + coord_flip() + scale_x_reverse()
 )
 
 
@@ -370,7 +419,7 @@ tables <- (wrap_elements(gridExtra::tableGrob(df_g)) + ggtitle("Survival")) +
   plot_spacer() +
   (wrap_elements(gridExtra::tableGrob(df_s)) + ggtitle("Growth"))
 
-ggplot2::ggsave(filename = "results/Correlation_s_g_variance_OPIM.png", 
+ggplot2::ggsave(filename = paste("results/simulations/correlation_between_s&g_", pars$species, "_variance.png", sep = ""), 
                 plot_var1 / plot_var2 / tables + plot_layout(heights = c(3,3,1)), width = 21, height =10)
 
 
@@ -386,7 +435,9 @@ d <- lapply(covariance, function(x) x %>%
               tibble::rowid_to_column(var = "X") %>% 
               gather(key = "Y", value = "cov", -1) %>%
               mutate(Y = as.numeric(gsub("V", "", Y))) %>%
-              ggplot(aes(X, Y, fill = cov)) + geom_tile() + theme_minimal() + coord_flip() + scale_x_reverse()
+              ggplot(aes(X, Y, fill = cov)) + geom_tile() + 
+              scale_fill_gradientn(colours = c("blue", "white", "red")) +
+              theme_minimal() + coord_flip() + scale_x_reverse()
 )
 
 plot_cov <- ((d$neg + ggtitle("-0.9")) + 
@@ -395,4 +446,6 @@ plot_cov <- ((d$neg + ggtitle("-0.9")) +
   plot_annotation(title = "Covariance between survivial and growth kernels",
                   subtitle = "with climate covariance")
 
-ggsave(filename = "results/Correlation_s_g_covariance_OPIM.png", plot_cov, width = 18, height = 7)
+ggsave(filename = paste("results/simulations/correlation_between_s&g_", pars$species, "_covariance.png", sep = ""), 
+                        plot_cov, width = 18, height = 7)
+
