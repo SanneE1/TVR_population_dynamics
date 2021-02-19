062c3b4d65811f0824938243655a2f1e32e2647c
Sanne <sanne.evers@idiv.de>
2021-02-19 14:47:02 +0100

    trying to get all years in one script

--- analysis/compadre_studies/retrieve_climate.R
+++ analysis/compadre_studies/retrieve_climate.R
@@ -12,7 +12,7 @@ library(RCurl)
 args     <- commandArgs(trailingOnly = TRUE)
 
 # climate variable
-job_n    <- c(1:12)
+job_n    <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 out_file <- args[1]
 
 # pipe-able Reduce_rbind and grep functions
@@ -24,9 +24,9 @@ rbind_l     <- function(df_list){ Reduce(function(...) rbind(...), df_list) }
 # coord
 coord_df <- read.csv('/data/lagged/species_authors.csv',
                      stringsAsFactors = F) %>% 
-              dplyr::select( Lat, Lon, SpeciesAuthor, MatrixPopulation ) %>% 
-              rename( Longitude = Lon,
-                      Latitude  = Lat )
+  dplyr::select( Lat, Lon, SpeciesAuthor, MatrixPopulation ) %>% 
+  rename( Longitude = Lon,
+          Latitude  = Lat )
 
 # site coord MATRIX (to feed "raster")
 site_coord <-  matrix(c(coord_df$Longitude,
@@ -42,24 +42,24 @@ site_coord <-  matrix(c(coord_df$Longitude,
 chelsa_df <- expand.grid( variable = c('prec', 'tmax', 'tmin'),
                           year     = c(1901:2019),
                           month    = c(paste0(1:9),
-                          # month    = c(paste0('0',1:9),
+                                       # month    = c(paste0('0',1:9),
                                        10,11,12),
                           stringsAsFactors = F) %>%
-                arrange(variable, year, month)
+  arrange(variable, year, month)
 
 # set up reading
 read_dir  <- 'https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V1/chelsa_cruts/'
 
 # produce file name based on index 'ii'
 produce_file_name <- function(ii){
-
+  
   paste0(chelsa_df$variable[ii],
          '/CHELSAcruts_',
          chelsa_df$variable[ii],'_',
          chelsa_df$month[ii],'_',
          chelsa_df$year[ii],'_',
          'V.1.0.tif')
-
+  
 }
 
 
@@ -68,91 +68,109 @@ set.seed( job_n )
 pp_v <- runif(1, 1, 200) %>% paste0()
 dir.create( pp_v )
 
-# get all file links (from file name)
-file_names <- sapply( job_n, produce_file_name)
-file_links <- paste0( read_dir, file_names)
-file_dest  <- paste0( pp_v, gsub('^prec','',file_names ) )
+# Create dataframe to save all data to
+all <- data.frame(Latitude = NA,	
+                  Longitude = NA,
+                  SpeciesAuthor = NA,
+                  MatrixPopulation = NA,	
+                  variable = NA,	
+                  year = NA,	
+                  month = NA,	
+                  value = NA)
 
 
-# 3. Extract CHELSA DATA ------------------------------------------------------------------------
-
-# download file
-file_path <- file_links
-
-# get the size of files BEFORE downloading them
-get_file_size <- function( file_path ){
+for (job_n in c(1:nrow(chelsa_df))) {
   
-  getURL(file_path, nobody=1L, header=1L) %>% 
-    strsplit(., "\r\n") %>% 
-    .[[1]] %>% 
-    grep( 'Content-Length: ',. ,value=T) %>% 
-    gsub( 'Content-Length: ','',. ) %>% 
-    as.numeric()
+  print(job_n)
+  # get all file links (from file name)
+  file_names <- sapply( job_n, produce_file_name)
+  file_links <- paste0( read_dir, file_names)
+  file_dest  <- paste0( pp_v, gsub('^prec','',file_names ) )
   
-} 
-
-
-# keep downloading until the file is actually downloaded
-test_file <- NULL
-file_s_t  <- TRUE
-
-
-while( length(test_file) == 0 & file_s_t ){
+  
+  # 3. Extract CHELSA DATA ------------------------------------------------------------------------
+  
+  # download file
+  file_path <- file_links
+  
+  # get the size of files BEFORE downloading them
+  get_file_size <- function( file_path ){
     
-  file_siz <- get_file_size(file_path)
+    getURL(file_path, nobody=1L, header=1L) %>% 
+      strsplit(., "\r\n") %>% 
+      .[[1]] %>% 
+      grep( 'Content-Length: ',. ,value=T) %>% 
+      gsub( 'Content-Length: ','',. ) %>% 
+      as.numeric()
+    
+  } 
   
-  # tryCatch does not end the process upon error
-  tryCatch(download.file(file_path, 
-                         file_dest, 
-                         mode = "wb"), 
-           error = function(e) print('http error 500 I assume')
-           )
-           
-  # test whether the file was downloaded or not
-  test_file <- grepl('.tif', list.files( pp_v ) )
-
-  # test whether downloaded file is large enough
-  file_n    <- grep('.tif', list.files( pp_v ), value = TRUE )
-  file_d_s  <- file.info( paste0( pp_v, '/', file_n) )$size
-  file_s_t  <- file_d_s != file_siz
   
-  # if file downloaded only partially, remove it and start over
-  if( file_s_t ){
-    file_r   <- grep('.tif$',list.files( pp_v ),value=T)
-    file.remove( paste0(pp_v, '/', file_r) )
+  # keep downloading until the file is actually downloaded
+  test_file <- NULL
+  file_s_t  <- TRUE
+  
+  
+  while( length(test_file) == 0 & file_s_t ){
+    
+    file_siz <- get_file_size(file_path)
+    
+    # tryCatch does not end the process upon error
+    tryCatch(download.file(file_path, 
+                           file_dest, 
+                           mode = "wb"), 
+             error = function(e) print('http error 500 I assume')
+    )
+    
+    # test whether the file was downloaded or not
+    test_file <- grepl('.tif', list.files( pp_v ) )
+    
+    # test whether downloaded file is large enough
+    file_n    <- grep('.tif', list.files( pp_v ), value = TRUE )
+    file_d_s  <- file.info( paste0( pp_v, '/', file_n) )$size
+    file_s_t  <- file_d_s != file_siz
+    
+    # if file downloaded only partially, remove it and start over
+    if( file_s_t ){
+      file_r   <- grep('.tif$',list.files( pp_v ),value=T)
+      file.remove( paste0(pp_v, '/', file_r) )
+    }
+    
   }
+  
+  
+  # get climate information
+  
+  # read raster file
+  print( 'read.raster.file' )
+  raster_file <- grep('.tif',list.files( pp_v ), value=T)
+  if( length(raster_file) > 1 ) warning( "More than one download in this dir" )
+  repP        <- raster( paste0( pp_v, '/', raster_file ) )
+  
+  # extract info from raster file
+  print( 'extract.raster.file' )
+  values_clim <- raster::extract(repP, site_coord, method = 'bilinear')
+  clim_df     <- coord_df %>%
+    mutate( variable = chelsa_df$variable[job_n],
+            year     = chelsa_df$year[job_n],
+            month    = chelsa_df$month[job_n],
+            value    = values_clim)
+  
+  # remove file
+  print( 'remove.download' )
+  file_r   <- grep('.tif$',list.files( pp_v ),value=T)
+  file.remove( paste0(pp_v, '/', file_r) )
+  
+  # remove temp directory
+  file.remove( pp_v, recursive=T )
+  
+  all <- rbind(all, clim_df)
   
 }
 
 
-# get climate information
-
-# read raster file
-print( 'read.raster.file' )
-raster_file <- grep('.tif',list.files( pp_v ), value=T)
-if( length(raster_file) > 1 ) warning( "More than one download in this dir" )
-repP        <- raster( paste0( pp_v, '/', raster_file ) )
-
-# extract info from raster file
-print( 'extract.raster.file' )
-values_clim <- raster::extract(repP, site_coord, method = 'bilinear')
-clim_df     <- coord_df %>%
-                  mutate( variable = chelsa_df$variable[job_n],
-                          year     = chelsa_df$year[job_n],
-                          month    = chelsa_df$month[job_n],
-                          value    = values_clim)
-
-# remove file
-print( 'remove.download' )
-file_r   <- grep('.tif$',list.files( pp_v ),value=T)
-file.remove( paste0(pp_v, '/', file_r) )
-
-# remove temp directory
-file.remove( pp_v, recursive=T )
-
-
 # put it all out
-write.csv(clim_df,
+write.csv(all,
           file.path(out_file, 'All_populations.csv'),
           row.names=F)
 
