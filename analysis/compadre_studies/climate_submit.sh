0ae28d1aab2246528d6c9eaafc1c9e4e321c022b
Sanne <sanne.evers@idiv.de>
2021-02-19 13:26:50 +0100

    initial script to try retrieval chelsa data using EVE

---
+++ analysis/compadre_studies/climate_submit.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -D /work/evers/
+
+#SBATCH -o /work/%u/%x-%A-%a.log
+
+#Specify job name
+#SBATCH -J mpms_compadre
+
+#Resources
+# max running time
+
+
+# memory per core (hard limit)
+#SBATCH --mem-per-cpu=8G
+
+# Array numbers 
+
+# create output direcotry per job
+OUTPUT_PATH="/work/$USER/$SLURM_JOB_NAME-$SLURM_ARRAY_JOB_ID"
+mkdir -p "$OUTPUT_PATH"
+
+# Load modules
+module load foss/2019b R/4.0.0-2
+
+
+Rscript "$HOME"/lagged_buffering/analysis/compadre_studies/retrieve_climate.R \
+"$OUTPUT_PATH"
