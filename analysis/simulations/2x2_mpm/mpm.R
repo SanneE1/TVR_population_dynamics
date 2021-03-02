38913b98204723ef9a87507d3db5b8d6d322a51b
Sanne <sanne.evers@idiv.de>
2021-03-02 10:33:07 +0100

    include different climate signal strengths and random noise in simulations

--- analysis/simulations/2x2_mpm/mpm.R
+++ analysis/simulations/2x2_mpm/mpm.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(tidyr)
 library(popbio)
 library(pbapply)
 library(parallel)
@@ -30,25 +31,25 @@ create_seq <- function(n_it, clim_sd, clim_corr, lag) {
 }
 
 
-mpm <- function(survival, growth, reproduction) {
+mpm <- function(survival, growth, reproduction, clim_sd, sig.strength = 1) {
   ## Basic mpm
   mpm <- matrix(0, nrow = 2, ncol = 2)
   
-  #growth
-  mpm[2,1] <- inv_logit(growth)  ### inv_logit(0) = 0.5 (intercept)
+  #growth                     Get the error term sd to reflect the sd of the environment sequence
+  mpm[2,1] <- inv_logit(growth * sig.strength + ((1-sig.strength) * rnorm(1, 0, clim_sd)) ) ### inv_logit(0) = 0.5 (intercept)
   
   # survival
-  mpm[2,2] <- inv_logit(survival)
+  mpm[2,2] <- inv_logit(survival + ((1-sig.strength) * rnorm(1, 0, clim_sd)) )
   
   # reproduction 
-  mpm[1,2] <- exp(1.2 + reproduction)
+  mpm[1,2] <- exp(1.2 + reproduction + ((1-sig.strength) * rnorm(1, 0, clim_sd)) )
   
   return(mpm)  
 }
 
 # "Stocastic" mpm -----------------------------------
 
-st.lamb <- function(env_surv, env_growth, env_reproduction) {
+st.lamb <- function(env_surv, env_growth, env_reproduction, clim_sd, sig.strength) {
   
   n_it = length(env_surv)
   
@@ -60,16 +61,18 @@ st.lamb <- function(env_surv, env_growth, env_reproduction) {
   env <- as.list(as.data.frame(t(env)))
   
   ### Get all mpm's
-  mats <- lapply(env, function(x) mpm(x[1], x[2], x[3]))
+  mats <- lapply(env, function(x) mpm(x[1], x[2], x[3], clim_sd, sig.strength))
   
   
   df = stoch.growth.rate(mats, maxt = 1000)$sim
   
-  return(df)
+  return(list(df = df,
+              mats = data.frame(t(lapply(mats, as.vector) %>% bind_rows)) %>% 
+                `colnames<-`(c("1,1", "2,1", "1,2", "2,2"))))
 }
 
 
-for(i in c(-1,1)) {   #### i means that I can run the same code for different climate effects without manual changes
+for(i in c(1, 0.5, 0.25, 0.05)) {   #### i means that I can run the same code for different climate effect strength without manual changes
   
   print(i)
   
@@ -91,19 +94,23 @@ for(i in c(-1,1)) {   #### i means that I can run the same code for different cl
   
 
   auto <- pblapply(cl = cl,
-                   as.list(c(1:900)),
-                   function(x) st.lamb(env_surv = rep(0, 50000) * i,
-                                       env_growth = create_seq(n_it = 50000, clim_sd[x], clim_corr[x], 0) * i,
-                                       env_reproduction = rep(0,50000) * i)
+                   as.list(c(1:225)),
+                   function(x) try(st.lamb(env_surv = rep(0, 50000),
+                                       env_growth = create_seq(n_it = 50000, clim_sd[x], clim_corr[x], 0)$recent,
+                                       env_reproduction = rep(0,50000),
+                                       clim_sd = clim_sd[x],
+                                       sig.strength = i))
   )
-
-  saveRDS(auto, paste("results/simulations/mpm/mpm_", i, "_auto.RDS", sep = ""))
+  
+  auto_df <- lapply(auto, function(x) x$df) %>% unlist
+  
+  saveRDS(auto_df, paste("results/simulations/mpm/mpm_", i, "_auto.RDS", sep = ""))
 
 
 
   ### Covariance all vr
   clim <- lapply(as.list(c(1:900)), function(x)
-    rnorm_multi(n = 50000,
+    rnorm_multi(n = 5000,
                 vars = 2,
                 mu = c(0,0),
                 sd = clim_sd[x],
@@ -114,41 +121,53 @@ for(i in c(-1,1)) {   #### i means that I can run the same code for different cl
 
   cov <- pblapply(cl = cl,
                   clim,
-                  function(x) st.lamb(env_surv = x$surv * i,
-                                      env_growth = x$growth * i,
-                                      env_reproduction = rep(0,50000) * i)
+                  function(x) st.lamb(env_surv = x$surv,
+                                      env_growth = x$growth,
+                                      env_reproduction = rep(0,5000),
+                                      clim_sd = sd(x$surv),
+                                      sig.strength = i)
   )
-
-  saveRDS(cov, paste("results/simulations/mpm/mpm_", i, "_cov.RDS", sep = ""))
+  cov_df <- lapply(cov, function(x) x$df) %>% unlist
+  
+  saveRDS(cov_df, paste("results/simulations/mpm/mpm_", i, "_cov.RDS", sep = ""))
 
   #### Lagged effect within P "functions"
   
-  lag_clim <- lapply(as.list(c(1:900)), function(x) create_seq(50000, clim_sd = clim_sd[x], clim_corr = clim_corr[x], lag = 1))
+  lag_clim <- lapply(as.list(c(1:900)), function(x) create_seq(5000, clim_sd = clim_sd[x], clim_corr = clim_corr[x], lag = 1))
   
   clusterExport(cl, c("lag_clim"))
   
   lag_g <- pblapply(cl = cl,
                     lag_clim,
-                    function(x) st.lamb(env_surv = x$recent * i,
-                                        env_growth = x$lagged * i,
-                                        env_reproduction = rep(0,length(x$recent)) * i)
+                    function(x) st.lamb(env_surv = x$recent,
+                                        env_growth = x$lagged,
+                                        env_reproduction = rep(0,length(x$recent)),
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
 
   lag_s <- pblapply(cl = cl,
                     lag_clim,
-                    function(x) st.lamb(env_surv = x$lagged * i,
-                                        env_growth = x$recent * i,
-                                        env_reproduction = rep(0,length(x$recent)) * i)
+                    function(x) st.lamb(env_surv = x$lagged,
+                                        env_growth = x$recent,
+                                        env_reproduction = rep(0,length(x$recent)),
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
 
   lag_n <- pblapply(cl = cl,
                     lag_clim,
-                    function(x) st.lamb(env_surv = x$recent * i,
-                                        env_growth = x$recent * i,
-                                        env_reproduction = rep(0,length(x$recent)) * i)
+                    function(x) st.lamb(env_surv = x$recent,
+                                        env_growth = x$recent,
+                                        env_reproduction = rep(0,length(x$recent)),
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
-
-  lag <- list("growth" = lag_g, "survival" = lag_s, "none" = lag_n)
+  lagg_df <- lapply(lag_g, function(x) x$df) %>% unlist
+  lags_df <- lapply(lag_s, function(x) x$df) %>% unlist
+  lagn_df <- lapply(lag_n, function(x) x$df) %>% unlist
+  
+  lag <- list("growth" = lagg_df, "survival" = lags_df, "none" = lagn_df)
 
   saveRDS(lag, paste("results/simulations/mpm/mpm_", i, "_lag.RDS", sep = ""))
 
@@ -156,28 +175,38 @@ for(i in c(-1,1)) {   #### i means that I can run the same code for different cl
   
   lag_p <- pblapply(cl = cl, 
                     lag_clim, 
-                    function(x) st.lamb(env_surv = x$lagged * i,
-                                        env_growth = x$lagged * i,
-                                        env_reproduction = x$recent * i)
+                    function(x) st.lamb(env_surv = x$lagged,
+                                        env_growth = x$lagged,
+                                        env_reproduction = x$recent,
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
   
   lag_f <- pblapply(cl = cl, 
                     lag_clim, 
-                    function(x) st.lamb(env_surv = x$recent * i,
-                                        env_growth = x$recent * i,
-                                        env_reproduction = x$lagged * i)
+                    function(x) st.lamb(env_surv = x$recent,
+                                        env_growth = x$recent,
+                                        env_reproduction = x$lagged,
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
   
-  lag_n <- pblapply(cl = cl, 
+  lag_n2 <- pblapply(cl = cl, 
                     lag_clim, 
-                    function(x) st.lamb(env_surv = x$recent * i,
-                                        env_growth = x$recent * i,
-                                        env_reproduction = x$recent * i)
+                    function(x) st.lamb(env_surv = x$recent,
+                                        env_growth = x$recent,
+                                        env_reproduction = x$recent,
+                                        clim_sd = sd(x$recent, na.rm = T),
+                                        sig.strength = i)
   )
+  lagp_df <- lapply(lag_p, function(x) x$df) %>% unlist
+  lagf_df <- lapply(lag_f, function(x) x$df) %>% unlist
+  lagn2_df <- lapply(lag_n2, function(x) x$df) %>% unlist
   
-  lag_fp <- list("Pkernel" = lag_p, "Fkernel" = lag_f, "none" = lag_n)
+  lag_fp <- list("Pkernel" = lagp_df, "Fkernel" = lagf_df, "none" = lagn2_df)
   
   saveRDS(lag_fp, paste("results/simulations/mpm/mpm_", i, "_lagfp.RDS", sep = ""))
   
   stopCluster(cl)
+
 }
