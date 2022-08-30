0797c426b699e74b9a568501d464aa37276f6259
Sanne <42802773+SanneE1@users.noreply.github.com>
2022-08-30 17:39:32 +0200

    simplify elasticity calculations

--- R/life_histories_functions.R
+++ R/life_histories_functions.R
@@ -7,14 +7,14 @@ create_mpm_from_vr <- function(df) {
     mat <- matrix(0,2,2)
     mat[1,1] <- df$Sj*(1-df$gamma)
     mat[2,1] <- df$Sj*df$gamma
-    mat[1,2] <- df$phi
+    mat[1,2] <- df$rho
     mat[2,2] <- df$Sa
   } else {
-    mat <- lapply(as.list(1:nrow(df)), function(x) {
+    mat <- lapply(seq_len(nrow(df)), function(x) {
       mat <- matrix(0,2,2)
       mat[1,1] <- df$Sj[x]*(1-df$gamma[x])
       mat[2,1] <- df$Sj[x]*df$gamma[x]
-      mat[1,2] <- df$phi[x]
+      mat[1,2] <- df$rho[x]
       mat[2,2] <- df$Sa[x]
       mat
     } )
@@ -29,25 +29,15 @@ single_row_elas <- function(ii) {
     add_column(elas_Sj = NA,
                elas_Sa = NA,
                elas_gamma = NA,
-               elas_phi = NA)
-  pert <- 0.01
-  
-  for(vr in c("Sj", "Sa", "phi", "gamma")) {
-    
-    df_h <- df_l <- df
-    df_h[,vr] <- df_h[, vr] + pert
-    df_l[,vr] <- df_l[, vr] - pert
-    
-    mean <- mat_df[ii,] %>% create_mpm_from_vr(.) %>% popbio::lambda(.)
-    high <- create_mpm_from_vr(df_h) %>% popbio::lambda(.)
-    low <- create_mpm_from_vr(df_l) %>% popbio::lambda(.)
-    
-    sens <- (high - low) / (2 * pert)
-    elas <- sens * abs(df[,vr]) / mean
-    
-    df[grep(paste0("elas_", vr), colnames(df))] <- elas
-    
-  }
+               elas_rho = NA)
+
+  matrix.elements <- expression(Sj*(1-gamma),  rho,
+                                Sj*gamma,      Sa)
+  vr_sens_elas <- popbio::vitalsens(matrix.elements, list(Sj = df$Sj,
+                                                          Sa = df$Sa,
+                                                          gamma = df$gamma,
+                                                          rho = df$rho) )
+  df[,c(5:8)] <- t(vr_sens_elas$elasticity)
   
   return(df)
 }
@@ -61,4 +51,8 @@ df_elas_per_vr <- function(vr_df) {
 # Calculate the maximum possible coefficient of variation for a probability (Morris & Doak 2004)
 CVmax <- function(mean) {
   sqrt((1-mean)/mean)
+}
+
+Vmax <- function(mean) {
+  mean*(1-mean)
 }
